67b7eba2c9eada9125cab16220872327e6675b65
furu04 <133187748+furu04@users.noreply.github.com>
2023-08-22 21:12:23 +0900

    fix slight orthographical variants

--- register.py
+++ register.py
@@ -9,7 +9,7 @@ def create_sqlite3():
         c = conn.cursor()
         # ログイン履歴は別テーブルに格納する
         c.execute('CREATE TABLE user (mailaddr, password, salt, regdate, regip)')
-        c.execute('CREATE TABLE login_history (mailaddr, login_date, login_ip)')
+        c.execute('CREATE TABLE login_history (mailaddr, logindate, loginip)')
         conn.commit()
         conn.close()
 
