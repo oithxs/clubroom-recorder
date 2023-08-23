59f68277596d5a819bcf76eac1df55982cc88f7e
furu04 <133187748+furu04@users.noreply.github.com>
2023-08-23 11:07:13 +0900

    fix indent

--- init.py
+++ init.py
@@ -1,10 +1,10 @@
 import sqlite3
 
 def db():
-        conn = sqlite3.connect('user.db')
-        c = conn.cursor()
-        # ログイン履歴は別テーブルに格納する
-        c.execute('CREATE TABLE user (mailaddr, password, salt, regdate, regip)')
-        c.execute('CREATE TABLE login_history (mailaddr, logindate, loginip)')
-        conn.commit()
-        conn.close()
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    # ログイン履歴は別テーブルに格納する
+    c.execute('CREATE TABLE user (mailaddr, password, salt, regdate, regip)')
+    c.execute('CREATE TABLE login_history (mailaddr, logindate, loginip)')
+    conn.commit()
+    conn.close()
