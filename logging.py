af4a0d90a6ec94f4e4adc29d057f2f263fee519a
furu04 <133187748+furu04@users.noreply.github.com>
2023-08-24 23:28:21 +0900

    minor fix: change DB insert statement

--- logging.py
+++ logging.py
@@ -3,5 +3,5 @@ import sqlite3
 def log(mailaddr, logindate, loginip, authresult):
     conn = sqlite3.connect('user.db')
     c = conn.cursor()
-    c.execute('INSERT INTO login_history VALUES (?, ?, ?)', (mailaddr, logindate, loginip, authresult))
+    c.execute('INSERT INTO login_history VALUES (?, ?, ?, ?)', (mailaddr, logindate, loginip, authresult))
     conn.close()
