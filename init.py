016e1e63984260d7552e4ceca3275ebc2f192b72
furu04 <133187748+furu04@users.noreply.github.com>
2023-08-24 17:03:29 +0900

    fix: change 'login_history' to log auth result

--- init.py
+++ init.py
@@ -5,6 +5,6 @@ def db():
     c = conn.cursor()
     # ログイン履歴は別テーブルに格納する
     c.execute('CREATE TABLE user (mailaddr, password, salt, regdate, regip)')
-    c.execute('CREATE TABLE login_history (mailaddr, logindate, loginip)')
+    c.execute('CREATE TABLE login_history (mailaddr, logindate, loginip, authresult)')
     conn.commit()
     conn.close()
