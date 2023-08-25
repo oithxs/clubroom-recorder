fe28b42f6dcd800e7a45eedf845060d544a350ba
hittowsl <68594072+hitto-hub@users.noreply.github.com>
2023-08-25 21:05:15 +0900

    clean

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,13 +1,14 @@
 from flask import Blueprint,request
+# import CRlogging
+from app.lib import log
 import sqlite3
 import bcrypt
 import datetime
-import CRlogging
 import jsonify
 
-login = Blueprint('login', __name__)
+blueprint = Blueprint('login', __name__)
 
-@login.route('/login', methods=['POST'])
+@blueprint.route('/login', methods=['POST'])
 def login():
     postdata = request.get_json()
     mailaddr = postdata['mailaddr']
@@ -18,24 +19,24 @@ def login():
         loginip = request.headers.getlist('X-Forwarded-For')[0]
     else:
         loginip = request.remote_addr
-    
+
     conn = sqlite3.connect('user.db')
     c = conn.cursor()
     c.execute('SELECT * FROM user WHERE mailaddr = ?', (mailaddr,))
     result = c.fetchone()
     conn.close()
-    
+
     if result is None:
-        CRlogging.log(mailaddr, logindate, loginip, 'failed, user not found')
+        log(mailaddr, logindate, loginip, 'failed, user not found')
         return jsonify({'result': 'failed', 'reason': 'user not found or wrong password'}), 401
-    
+
     salt = result[2]
     password = salt + password.encode('utf-8')
     password = bcrypt.hashpw(password, salt)
-    
+
     if result[1] == password:
-        CRlogging.log(mailaddr, logindate, loginip, 'success')
+        log(mailaddr, logindate, loginip, 'success')
         return jsonify({'result': 'success'}), 200
     else:
-        CRlogging.log(mailaddr, logindate, loginip, 'failed, wrong password')
-        return jsonify({'result': 'failed', 'reason': 'user not found or wrong password'}), 401 
+        log(mailaddr, logindate, loginip, 'failed, wrong password')
+        return jsonify({'result': 'failed', 'reason': 'user not found or wrong password'}), 401
